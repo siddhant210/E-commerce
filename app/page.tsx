e77e9a92afa4f9d743caea29c31e36e25dfe21f5
Siddhantt06 <crce.9966.ce@gmail.com>
2026-06-29 12:34:20 +0530

    Day 8 Initial Project

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+export default function Home() {
+  return (
+    <main>
+      <Header />
+
+      <h2>Welcome to Our Store</h2>
+
+      <p>This is our E-Commerce Website.</p>
+
+      <Footer />
+    </main>
+  );
+}
---
+++ components/Header.tsx
@@ -0,0 +1,18 @@
+function Header() {
+  return (
+    <header>
+      <h1>E-Commerce Store</h1>
+
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Products</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
