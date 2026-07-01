1cd9d82c375a6b3d605e7a9b63ebd41cb6c6bb32
Siddhantt06 <crce.9966.ce@gmail.com>
2026-07-01 12:22:18 +0530

    Day 14  Initial Project

---
+++ app/products/page.tsx
@@ -0,0 +1,9 @@
+export default function ProductsPage() {
+  return (
+    <main>
+      <h1>Products Page</h1>
+
+      <p>This page will show all products.</p>
+    </main>
+  );
+}
---
+++ components/Navbar.tsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+
+function Navbar() {
+  return (
+    <nav>
+      <ul
+        style={{
+          display: "flex",
+          gap: "20px",
+          listStyle: "none",
+          padding: "20px",
+          backgroundColor: "#2563eb",
+        }}
+      >
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+
+        <li>
+          <Link href="/products">Products</Link>
+        </li>
+
+        <li>
+          <Link href="/about">About</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
